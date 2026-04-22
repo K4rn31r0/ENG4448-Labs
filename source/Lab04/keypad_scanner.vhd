a480f14fa4c238dcd028e64d355aa2bee8c71c79
Pedro Cunha <mr.pedrocunha@gmail.com>
2026-04-22 11:48:10 -0300

    Implement keypad scanner FSM and testbench

--- source/Lab04/keypad_scanner.vhd
+++ source/Lab04/keypad_scanner.vhd
@@ -7,13 +7,18 @@ entity keypad_scanner is
 			clk : in STD_LOGIC;
 			rst : in STD_LOGIC;
 			col : out  STD_LOGIC_VECTOR (3 downto 0);
-         row : in  STD_LOGIC_VECTOR (3 downto 0)		
+         row : in  STD_LOGIC_VECTOR (3 downto 0);
+			
+			key_code : out STD_LOGIC_VECTOR (3 downto 0);
+			key_pressed : out STD_LOGIC
 			);
 end keypad_scanner;
 
 architecture Behavioral of keypad_scanner is
 
 	signal scan_tick : STD_LOGIC;
+	signal last_scan_tick : STD_LOGIC := '0';
+   signal scan_enable : STD_LOGIC;
 	
 	type state_type is (SCAN_C1, SCAN_C2, SCAN_C3, SCAN_C4, WAIT_RELEASE);
 	signal state : state_type := SCAN_C1;
@@ -25,9 +30,119 @@ begin
 		Generic map (N => 19)
 		Port map (
 			clk => clk,
-			rst => rst,
+			clr => rst,
 			q => scan_tick
 		);
+	
+	col <= "0111" when state = SCAN_C1 else
+           "1011" when state = SCAN_C2 else
+           "1101" when state = SCAN_C3 else
+           "1110" when state = SCAN_C4 else
+           "0000"; 
+		
+	keypad_fsm : process(clk)
+	begin
+		
+		if rising_edge(clk) then
+			last_scan_tick <= scan_tick;
+			
+			if rst = '1' then
+				state <= SCAN_C1;
+				key_code <= "0000";
+				key_pressed <= '0';
+				
+			elsif (scan_tick = '1' and last_scan_tick = '0') then
+			
+				case state is
+				
+					when SCAN_C1 =>
+						if row /= "1111" then
+						
+							if row = "0111" then
+								key_code <= "0001";		-- 1
+							elsif row = "1011" then
+								key_code <= "0100";		-- 4
+							elsif row = "1101" then
+								key_code <= "0111";		-- 7
+							elsif row = "1110" then
+								key_code <= "0000";		-- 0
+							end if;
+							
+							key_pressed <= '1';
+							state <= WAIT_RELEASE;
+						else 
+							state <= SCAN_C2;
+						end if;
+					
+					when SCAN_C2 =>
+						if row /= "1111" then
+						
+							if row = "0111" then
+								key_code <= "0010";		-- 2
+							elsif row = "1011" then
+								key_code <= "0101";		-- 5
+							elsif row = "1101" then
+								key_code <= "1000";		-- 8
+							elsif row = "1110" then
+								key_code <= "1111";		-- F
+							end if;
+							
+							key_pressed <= '1';
+							state <= WAIT_RELEASE;
+						else 
+							state <= SCAN_C3;
+						end if;
+						
+					when SCAN_C3 =>
+						if row /= "1111" then
+						
+							if row = "0111" then
+								key_code <= "0011";		-- 3
+							elsif row = "1011" then
+								key_code <= "0110";		-- 6
+							elsif row = "1101" then
+								key_code <= "1001";		-- 9
+							elsif row = "1110" then
+								key_code <= "1110";		-- E
+							end if;
+							
+							key_pressed <= '1';
+							state <= WAIT_RELEASE;
+						else 
+							state <= SCAN_C4;
+						end if;
+						
+					when SCAN_C4 =>
+						if row /= "1111" then
+						
+							if row = "0111" then
+								key_code <= "1010";		-- A
+							elsif row = "1011" then
+								key_code <= "1011";		-- B
+							elsif row = "1101" then
+								key_code <= "1100";		-- C
+							elsif row = "1110" then
+								key_code <= "1101";		-- D
+							end if;
+							
+							key_pressed <= '1';
+							state <= WAIT_RELEASE;
+						else 
+							state <= SCAN_C1;
+						end if;
+					
+					when WAIT_RELEASE =>
+					
+						if row = "1111" then
+							key_pressed <= '0';
+							state <= SCAN_C1;
+						end if;
+						
+				end case;
+			end if;
+		end if;
+	
+	end process keypad_fsm;
 
 end Behavioral;
 
---
+++ source/Lab04/keypad_scanner_test.vhd
@@ -0,0 +1,91 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+ 
+ENTITY keypad_scanner_test IS
+END keypad_scanner_test;
+ 
+ARCHITECTURE behavior OF keypad_scanner_test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT keypad_scanner
+    PORT(
+         clk : IN  std_logic;
+         rst : IN  std_logic;
+         col : OUT  std_logic_vector(3 downto 0);
+         row : IN  std_logic_vector(3 downto 0);
+         key_code : OUT  std_logic_vector(3 downto 0);
+         key_pressed : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal rst : std_logic := '0';
+   signal row : std_logic_vector(3 downto 0) := "1111";
+
+ 	--Outputs
+   signal col : std_logic_vector(3 downto 0);
+   signal key_code : std_logic_vector(3 downto 0);
+   signal key_pressed : std_logic;
+
+   -- Clock period definitions
+   constant clk_period : time := 20 ns;
+	
+	signal btn_5_pressed : std_logic := '0';
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: keypad_scanner PORT MAP (
+          clk => clk,
+          rst => rst,
+          col => col,
+          row => row,
+          key_code => key_code,
+          key_pressed => key_pressed
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+	
+	-- Logica combinacional fora dos processes
+	row(3) <= '1';
+   row(2) <= col(2) when btn_5_pressed = '1' else '1';
+   row(1) <= '1';
+   row(0) <= '1';
+ 
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- reseta o sistema
+      rst <= '1';
+		btn_5_pressed <= '0';
+		
+      wait for 100 ns;
+		
+      rst <= '0';
+      
+      -- deixa o scanner varrer um pouco no vazio
+      wait for 30 ms; 
+
+      -- dedo aperta a tecla '5'!
+      btn_5_pressed <= '1';
+      wait for 50 ms; -- Segura por 50 milissegundos
+
+      -- dedo solta a tecla
+      btn_5_pressed <= '0';
+      wait for 50 ms; -- Observa o sistema voltar ao normal
+
+      assert false report "FIM" severity failure;
+   end process;
+
+END;
