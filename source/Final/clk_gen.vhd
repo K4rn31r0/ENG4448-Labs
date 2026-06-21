077e2ee7141f6fea99549aa406f255b28b199298
Pedro Cunha <mr.pedrocunha@gmail.com>
2026-06-21 16:50:28 -0300

    Add clock divider module with a simple testbench

---
+++ source/Final/clk_gen.vhd
@@ -0,0 +1,37 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity clk_gen is
+    generic (
+        DIVISOR : positive := 50_000_000	-- 0.5 Hz (periodo 2s, 1 toggle/s)
+    );
+    port (
+        CLK50  : in  STD_LOGIC;
+        CLKOUT : out STD_LOGIC
+    );
+end clk_gen;
+
+architecture Behavioral of clk_gen is
+
+	-- 
+    signal cnt   : natural range 0 to DIVISOR-1 := 0;
+    signal clk_r : STD_LOGIC := '0';
+
+begin
+
+    process (CLK50)
+    begin
+        if rising_edge(CLK50) then
+			if cnt = DIVISOR-1 then
+				cnt   <= 0;
+                clk_r <= not clk_r;
+            else
+                cnt <= cnt + 1;
+            end if;
+        end if;
+    end process;
+
+    CLKOUT <= clk_r;
+
+end Behavioral;
---
+++ source/Final/clk_gen_tb.vhd
@@ -0,0 +1,63 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY clk_gen_tb IS
+END clk_gen_tb;
+ 
+ARCHITECTURE behavior OF clk_gen_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT clk_gen
+	GENERIC (
+        DIVISOR : positive := 50_000_000
+    );
+    PORT(
+         CLK50 : IN  std_logic;
+         CLKOUT : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal CLK50 : std_logic := '0';
+
+ 	--Outputs
+   signal CLKOUT : std_logic;
+
+   -- Clock period definitions
+   constant CLK50_period : time := 20 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: clk_gen 
+   GENERIC MAP (
+		DIVISOR => 8
+   )
+   PORT MAP (
+          CLK50 => CLK50,
+          CLKOUT => CLKOUT
+        );
+
+   -- Clock process definitions
+   CLK50_process :process
+   begin
+		CLK50 <= '0';
+		wait for CLK50_period/2;
+		CLK50 <= '1';
+		wait for CLK50_period/2;
+   end process;
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      wait until rising_edge(clkout);
+	  report "Primeira borda";
+	  wait until falling_edge(clkout);
+	  report "Segunda borda";
+	  assert false report "END" severity failure;
+   end process;
+
+END;
